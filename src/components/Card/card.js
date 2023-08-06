8f304eb661269335608006e857b379e12485c641
Yuliia Barabash <yuliiabarabash@MacBook-Pro-Yuliia.local>
2023-08-07 01:44:57 +0300

    production table

--- src/components/Card/card.js
+++ src/components/Card/card.js
@@ -6,7 +6,7 @@ import "./card.css";
 const Card = () => {
   return (
     <div className="login-card">
-      <img src={logo} className="login-logo" alt="login page logo" />
+      <img src={logo} className="login-logo" alt="main logo" />
       <form className="input-container">
           <Input shouldHide={false} />
           <Input shouldHide={true} />
