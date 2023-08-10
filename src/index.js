99f12bf626fdef5af5e3081bf2afa1caf46dbc2c
RiaaB <119138826+YuliiaBarabash@users.noreply.github.com>
2023-08-10 23:55:59 +0300

    Revert "production table"

--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Login from './containers/Login/login';
-import Products from './containers/Products/products';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   {/* <Login /> */}
-   <Products />
+   <Login />
   </React.StrictMode>
 );
 
